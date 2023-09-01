2f36b89063798b726f24b15056281e6290b32a28
ShigemoriHakura <62388797+ShigemoriHakura@users.noreply.github.com>
2023-09-02 07:14:50 +0800

    [init] upload all codes

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # TRTC_Console_Wrapper
 A wrapper which only returns your devices info in json so the fuc*ing trtc-c-wrapper will not crash unity
+
+#About
+Just compile and use
---
+++ Shiro_Trtc_Wrapper_x64/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using ManageLiteAV;
+using Newtonsoft.Json;
+using System.Runtime.InteropServices;
+using System.Collections.Generic;
+
+namespace Shiro_Trtc_Wrapper_x64
+{
+    class Program
+    {
+        public ITRTCCloud mTRTCCloud;
+
+        static void Main(string[] args)
+        {
+            var mTRTCCloud = ITRTCCloud.getTRTCShareInstance();
+            mTRTCCloud.setLogLevel(TRTCLogLevel.TRTCLogLevelNone);
+            //Console.WriteLine("============ Starting Echo ============ ");
+
+            var manager = mTRTCCloud.getDeviceManager();
+
+
+            var returnedJson = new TXJson
+            {
+                Cameras = GetAllDevicesFromCollection(manager.getDevicesList(TXMediaDeviceType.TXMediaDeviceTypeCamera)),
+                Mics = GetAllDevicesFromCollection(manager.getDevicesList(TXMediaDeviceType.TXMediaDeviceTypeMic)),
+                Speakers = GetAllDevicesFromCollection(manager.getDevicesList(TXMediaDeviceType.TXMediaDeviceTypeSpeaker))
+            };
+            Console.WriteLine(JsonConvert.SerializeObject(returnedJson));
+        }
+
+        public static List<TXDevice> GetAllDevicesFromCollection(ITRTCDeviceCollection devices)
+        {
+            var ret = new List<TXDevice>();
+            for (uint i = 0; i < devices.getCount(); i++)
+            {
+                ret.Add(GetDevice(devices, i));
+            }
+            return ret;
+        }
+
+        public static TXDevice GetDevice(ITRTCDeviceCollection devices, uint i)
+        {
+            var device = new TXDevice
+            {
+                DeviceName = devices.getDeviceName(i),
+                DevicePID = devices.getDevicePID(i)
+            };
+            return device;
+        }
+
+        public struct TXDevice
+        {
+            public String DevicePID;
+            public String DeviceName;
+        }
+
+        public struct TXJson
+        {
+            public List<TXDevice> Cameras;
+            public List<TXDevice> Mics;
+            public List<TXDevice> Speakers;
+        }
+    }
+}
